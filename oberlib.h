33e65b91afccb317e37fff06a5d2b0feaf9824e9
Mason Back <masonleeback@gmail.com>
2023-06-11 16:08:41 -0400

    Initial commit of non-matching code

---
+++ oberlib.h
@@ -0,0 +1,26 @@
+/*
+
+OberEngine Decompilation
+Original Game: Purble Place
+
+*/
+
+#ifndef _OBERLIB_H_
+#define _OBERLIB_H_
+
+#include "cabinet.h"
+#include "commoncontroller.h"
+#include "dialoghelper.h"
+#include "dllfilemgr.h"
+#include "localize.h"
+#include "logger.h"
+#include "serializabledata.h"
+#include "shareddialogs.h"
+#include "sqmtimerecorder.h"
+#include "str.h"
+#include "textureloader.h"
+#include "thread.h"
+#include "utils.h"
+#include "xmlnode.h"
+
+#endif // _OBERLIB_H_
---
+++ str.h
@@ -0,0 +1,45 @@
+/*
+
+OberEngine Decompilation
+Original Game: Purble Place
+
+*/
+
+#ifndef _STR_H_
+#define _STR_H_
+
+#define STR_MAX_LENGTH 0xFFFFF
+
+class Str
+{
+private:
+  size_t m_iLength; // 0
+  size_t m_iCount; // 1
+  wchar_t* m_pStr; // 2
+
+public:
+  Str(Str* other);
+  Str(const wchar_t* other);
+  Str(wchar_t* Source, size_t Length);
+  Str(unsigned int resourceID);
+
+  ~Str();
+
+  Str* operator=(wchar_t* rhs);
+  Str* operator=(Str* rhs);
+  Str* operator+=(Str* rhs);
+  Str* operator+=(wchar_t* rhs);
+
+  bool Equals(const Str* other);
+  bool CIEquals(const Str* other);
+
+  void Clear();
+
+  size_t Format(wchar_t* Format, ...);
+  size_t AppendF(wchar_t* Format, ...);
+
+  Str* SubString(Str* result, size_t startIndex, size_t length);
+  static Str* FromNumber(int n);
+};
+
+#endif // _STR_H_
