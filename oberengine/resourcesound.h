de617c6c09f1ee91b982d6981f5d3531b1363dbf
Mason Back <masonleeback@gmail.com>
2023-10-12 23:09:09 -0400

    Add Get/SetVolume to the base resource sound class

--- oberengine/resourcesound.h
+++ oberengine/resourcesound.h
@@ -17,6 +17,9 @@ class ResourceSound : public ResourceBase
 public:
   ResourceSound();
   ~ResourceSound();
+
+  unsigned int GetVolume(unsigned int a1);
+  void SetVolume(unsigned int volume, unsigned int a2); // todo : figure out vars
 };
 
 #endif // _RESOURCESOUND_H_
